6fa8b913e9725a9afb31099fedbef22ec69ebb51
Edurios2002 <riosnavaluiseduardo@gmail.com>
2024-10-01 17:40:32 -0600

    Agregar archivos de proyecto.

---
+++ Practica 7/CommonValues.h
@@ -0,0 +1,8 @@
+#ifndef COMMONVALS
+#define COMMONVALS
+#include "stb_image.h"
+
+//para definir cuantas luces podemos tener.
+const int MAX_POINT_LIGHTS = 3;
+const int MAX_SPOT_LIGHTS = 4;
+#endif
